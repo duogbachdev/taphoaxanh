ec67515b941dd39cebb36b8c9011c8994473de52
duogbachdev <contact.duogbachdev@gmail.com>
2024-10-18 11:05:54 +0700

    Init Project

---
+++ shopapp-backend/src/main/java/com/project/shopapp/models/Role.java
@@ -0,0 +1,23 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+  public static String ADMIN = "ADMIN";
+  public static String USER = "USER";
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "name", nullable = false)
+  private String name;
+}
